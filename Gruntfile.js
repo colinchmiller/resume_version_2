43b30229d00cc6de0c16293a2a78df8c599639c6
colinmiller <colinchmiller@gmail.com>
2016-03-07 10:15:03 -0600

    add gruntfile and test success

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = (function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify:{
+      options: {
+                banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+      app: {
+        src: 'client/scripts/app.js',
+        dest: 'server/public/assets/scripts/app.min.js'
+      }
+    },
+    copy: {
+      html: {
+        expand: true,
+        cwd: 'client/views',
+        src: 'index.html',
+        dest: 'server/public/assets/views'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['copy', 'uglify']);
+});
